dda9c924aeedbe785399669693c66d34ca59843f
Reny Ramos <renyramosgarcia@gmail.com>
2019-07-04 19:09:17 -0400

    Modificada clase para permitir tener titulo en los mensajes.

--- src/LaravelMsg.php
+++ src/LaravelMsg.php
@@ -6,60 +6,66 @@ class Message
 
     static $typeFieldName    = "message_type";
     static $messageFieldName = "message";
+    static $titleFieldName   = "title";
 
     /**
      * Envia un mensaje de exito al front.
      *
+     * @param string $title
      * @param string $message
      * @return void
      */
-    public static function success(string $message = null)
+    public static function success(string $title = null, string $message = null)
     {
-        return self::message($message, 'success');
+        return self::message($title, $message, 'success');
     }
 
     /**
      * Envia un mensaje informativo al front
      *
+     * @param string $title
      * @param string $message
      * @return void
      */
-    public static function info(string $message = null)
+    public static function info(string $title = null, string $message = null)
     {
-        return self::message($message, 'info');
+        return self::message($title, $message, 'info');
     }
 
     /**
      * Envia un mensaje de Advertencia al front
      *
+     * @param string $title
      * @param string $message
      * @return void
      */
-    public static function warning(string $message = null)
+    public static function warning(string $title = null, string $message = null)
     {
-        return self::message($message, 'warning');
+        return self::message($title, $message, 'warning');
     }
 
     /**
      * Envia un mensaje de error al front.
      *
+     * @param string $title
      * @param string $message
      * @return void
      */
-    public static function error(string $message = null)
+    public static function error(string $title = null, string $message = null)
     {
-        return self::message($message, 'error');
+        return self::message($title, $message, 'error');
     }
 
     /**
      * Envia una pregunta al front (sin opcion de respuesta)
      *
+     * @param string $title
      * @param string $message
      * @return void
      */
-    public static function question(string $message = null)
+    public static function question(string $title = null, string $message = null)
     {
-        return self::message($message, 'question');
+        return self::message($title, $message, 'question');
     }
 
     /**
@@ -68,15 +74,19 @@ class Message
      * A pesar de que la utilizacion principal es para servir de ayuda a las demas funciones y no de utilizarse
      * sola, es posible utilizarla como esta para pasar algun tipo distinto de mensaje.
      *
+     * @param string $title
      * @param string $message
-     * @param [type] $type
+     * @param string $type
      * @return void
      */
-    public static function message(string $message = null, $type = null)
+    public static function message(string $title = null, string $message = null, string $type = 'success')
     {
-        if ($message && $type) {
-            \Session::flash(self::$typeFieldName, $type);
+        \Session::flash(self::$typeFieldName, $type);
+        if (isset($message)) {
+            \Session::flash(self::$titleFieldName, $title);
             \Session::flash(self::$messageFieldName, $message);
+        } else {
+            \Session::flash(self::$titleFieldName, $title);
         }
     }
 }
