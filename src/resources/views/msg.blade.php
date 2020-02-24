37d72d06ae522ab1e59050ff83474b5060852ccb
Reny Ramos <renyramosgarcia@gmail.com>
2020-02-24 16:53:34 -0400

    Nueva vista para mensajes

---
+++ src/resources/views/msg.blade.php
@@ -0,0 +1,20 @@
+@if (session('message_type'))
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+    <script>
+        function showMensage() {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 4000
+            });
+
+            Toast.fire({
+                type: "{{ session('message_type') }}",
+                title: "{{ session('title') }}",
+                text: "{{ session('message') }}"
+            })
+        };
+    showMensage();
+    </script>
+@endif
